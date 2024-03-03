f6ec8adadff21461440b7643d6a3a0c7df5941b3
reggmack <[200~21286023+reggmack@users.noreply.github.com~>
2024-03-03 14:45:05 -0600

    Add lodash, css style sheet, and bundle.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Test Pack Watched Again 5");
+import _ from 'lodash';
+import './styles.css';
+
+console.log("Test Pack Watched reverted");
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    //element.textContent = myName('Max');
+
+    element.classList.add('hello');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
